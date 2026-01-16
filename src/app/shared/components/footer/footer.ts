65a4bfb7749df34ec9823613777f84e254f76c23
R <rizkyramadhanpd@gmail.com>
2026-01-16 11:02:54 +0700

    style: enhance footer layout for improved responsiveness and visual consistency

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -4,11 +4,11 @@ import { Component, signal } from '@angular/core';
   selector: 'app-footer',
   imports: [],
   template: `
-    <footer class="flex flex-col justify-center items-center gap-y-2.5 text-base-foreground-300 w-full pt-10">
+    <footer class="flex flex-col justify-center items-center gap-y-2.5 text-base-foreground-300 text-sm/6 text-center w-full pt-10 sm:text-base/7">
       <p>&copy; {{ year() }} Rizky Ramadhan. All rights reserved.</p>
-      <div class="flex items-center gap-x-3">
-        <p>Designed by <a [href]="url()">{{ username() }}</a></p>
-        <svg viewBox="0 0 3 3" width="3" height="3" aria-hidden="true" class="fill-base-foreground-300">
+      <div class="flex flex-col items-center gap-x-3 gap-y-1 sm:flex-row">
+        <p>Designed by <a [href]="url()" class="capitalize">{{ username() }}</a></p>
+        <svg viewBox="0 0 3 3" width="3" height="3" aria-hidden="true" class="hidden fill-base-foreground-300 sm:block">
           <circle r="1" cx="1" cy="1" />
         </svg>
         <p>Powered by {{ license() }} License</p>
